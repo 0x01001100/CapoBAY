c41449cad28a2ab3ca6686c9d28ca5c91271ccfc
0x01001100 <86391643+0x01001100@users.noreply.github.com>
2023-03-30 18:13:08 +0200

    Create pm2-metrics.js

---
+++ nodejs-tools/pm2-metrics.js
@@ -0,0 +1,91 @@
+
+/*
+Licensed under the CapollBAY:CapollBot statements.
+*/
+
+/*
+This "command" would prompt / parse the (specified) information/metrics that the production process manager PM2 can return.
+*/
+
+//Specify logic statements like: Switches, if(){}else{} statements.
+
+//Command begin/process.flow:
+
+    exec('pm2 list', (error, stdlist, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}` );
+        return;
+      }
+
+
+    exec('pm2 info 0', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}` );
+        return;
+      }
+
+    let percentStrings = stdlist
+    .split(" ")
+    .filter(word => word.includes("%"))
+    .map(str => str.toUpperCase());
+
+    let mbStrings = stdlist
+    .split(" ")
+    .filter(word => word.includes("mb"))
+    .map(str => str.toUpperCase());
+
+     let input = stdout;
+      
+     const lines = stdout.split('\n');
+      
+     let restartsLine = lines.find(line => line.startsWith('│ restarts'));
+     let uptimeLine = lines.find(line => line.startsWith('│ uptime'));
+
+     let restarts = restartsLine.split('│')[2].trim();
+     let uptime = uptimeLine.split('│')[2].trim();
+
+    
+     if(uptime.includes('D') ){uptime = simple_dcounter }
+
+
+    /* 
+    const codeMetrics = lines
+    .slice(codeMetricsIndex + 2, codeMetricsIndex + 9)
+    .map((line) => line.split("│").map((value) => value.trim()).filter((value) => value !== ""));
+*/
+
+
+const codeMetricsIndex = lines.findIndex((line) => line.startsWith(" Code metrics value"));
+
+const [usedHeapSizeLine, heapUsageLine, heapSizeLine, eventLoopLatencyP95Line, eventLoopLatencyLine, activeHandlesLine, activeRequestsLine] = lines.slice(codeMetricsIndex + 2, codeMetricsIndex + 9).map((line) => line.trim().split("│"));
+
+const usedHeapSize = usedHeapSizeLine[2].trim();
+const heapUsage = heapUsageLine[2].trim();
+const heapSize = heapSizeLine[2].trim();
+const eventLoopLatencyP95 = eventLoopLatencyP95Line[2].trim();
+const eventLoopLatency = eventLoopLatencyLine[2].trim();
+const activeHandles = activeHandlesLine[2].trim();
+const activeRequests = activeRequestsLine[2].trim();
+
+console.log(`Used Heap Size: ${usedHeapSize}`);
+console.log(`Heap Usage: ${heapUsage}`);
+console.log(`Heap Size: ${heapSize}`);
+console.log(`Event Loop Latency p95: ${eventLoopLatencyP95}`);
+console.log(`Event Loop Latency: ${eventLoopLatency}`);
+console.log(`Active handles: ${activeHandles}`);
+console.log(`Active requests: ${activeRequests}`);
+
+      
+//For the Discord.js Bot:       
+
+    let capostatus  = new EmbedBuilder()
+    .setColor('#50c878') 
+    .setTitle('                          **CapollBot**    *Process Status*          ')
+
+    .setDescription(`\n\n  *CapoEntity Process:*  \n\n    ***CPU***:     ${percentStrings}   \n    ***Memoria***:     **${mbStrings}** \n\n  **Heap Usage: ${heapUsage}** \n\n Used Heap Size: ${usedHeapSize} \n(Heap Size: ${heapSize})   \n\n\n  **Event Loop Latency: ${eventLoopLatency}**  \n\n *Temps Viu:*  **${uptime}**  \n *Reinicis desde l'últim deploy*: ${restarts}  `)
+    .setFooter({
+    text: `CapollBot® : ElServidorDelsCapolls`,
+    iconURL: `https://cdn.discordapp.com/emojis/918724032415813692.png`,  });
+
+    message.channel.send({  embeds: [capostatus]  });
+
