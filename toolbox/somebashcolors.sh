b9c68455de65e5ff07a767153459b6037e23df8a
L <86391643+0x01001100@users.noreply.github.com>
2024-02-01 16:53:43 +0100

    somebashcolors.sh

---
+++ toolbox/somebashcolors.sh
@@ -0,0 +1,33 @@
+# http://es.tldp.org/COMO-INSFLUG/COMOs/Bash-Prompt-Como/Bash-Prompt-Como-5.html
+
+#ColorCodes
+red='\033[0;31m'
+green='\033[0;32m'
+blue='\033[0;34m'
+yellow='\033[0;33m'
+morat='\033[0;35m'
+cyan='\033[0;36m'
+white='\033[0;37m'
+black='\033[0;30m'
+
+# BACKGROUND_COLORS
+
+fondo_negro='\033[0;40m'
+fondo_rojo='\033[0;41m'
+fondo_verde='\033[0;42m'
+fondo_amarillo='\033[0;43m'
+fondo_azul='\033[0;44m'
+fondo_purpura='\033[0;45m'
+fondo_cian='\033[0;46m'
+fondo_blanco='\033[0;47m'
+
+# Estilos de texto
+negrita='\033[0;1m'
+desvanecido='\033[0;2m'
+subrayado='\033[0;4m'
+parpadeo='\033[0;5m'
+inverso='\033[0;7m'
+
+reset='\033[0m' # Reset -> volver al color predeterminado
+
+
